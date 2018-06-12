95eaeb46061fe908ac0a32e8cba2b03c469c981e
ranqiqiang <qiqiang@ran.gmail.com>
2018-06-12 14:57:44 +0800

    增加端域名生成器

---
+++ src/main/java/com/dfire/util/ShortUrlUtil.java
@@ -0,0 +1,55 @@
+package com.dfire.util;
+
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+
+/**
+ * @author <a href="mailto:huoguo@2dfire.com">火锅</a>
+ * @time 18/6/12
+ */
+public class ShortUrlUtil extends UDF {
+
+
+    /**
+     *  @排骨 @冰石 负责
+     */
+    public static final String CODE_STRING = "vPh7zZwA2LyU4bGq5tcVfIMxJi6XaSoK9CNp0OWljYTHQ8REnmu31BrdgeDkFs";
+
+    /**
+     * 数字转短网址
+     *
+     * @param lng
+     * @return 短网址字符串
+     */
+    public static String encode(Long lng) {
+        String out = "";
+        double val = lng;
+        for (double t = Math.floor(Math.log10(val) / Math.log10(62)); t >= 0; t--) {
+            double a = Math.floor(val / Math.pow(62, t));
+            out = out + CODE_STRING.substring((int) a, (int) a + 1);
+            val = val - (a * Math.pow(62, t));
+        }
+        return out;
+    }
+
+    /**
+     * 短网址转数字
+     *
+     * @param str
+     * @return 数字字符串
+     */
+    public static Long decode(String str) {
+        double out = 0;
+        int len = str.length() - 1;
+        for (int t = 0; t <= len; t++) {
+            out = out + CODE_STRING.indexOf(str.substring(t, t + 1)) * Math.pow(62, len - t);
+        }
+        return (long) out;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ShortUrlUtil.encode(200435L));
+    }
+
+}
