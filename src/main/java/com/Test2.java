e964efb1879aff40bbb3ff5a339d8f52e7c2f7c3
ranqiqiang <qiqiang@ran.gmail.com>
2019-05-29 10:28:30 +0800

    0.4 临时加个 16进制处理的字符

--- src/main/java/com/Test2.java
+++ src/main/java/com/Test2.java
@@ -1,6 +1,10 @@
 package com;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author <a href="mailto:huoguo@2dfire.com">火锅</a>
@@ -10,7 +14,7 @@ public class Test2 {
 
     private static String hexString = "0123456789ABCDEF";
 
-    public static void main(String[] args) throws UnsupportedEncodingException {
+    public static void main(String[] args) throws IOException {
         String s = "\\x22\\xE7\\x89\\x9B\\xE6\\xB2\\xB9\\xE6\\x9E\\x9C\\x22";
         String s1 = "\\xe6\\x88\\x91\\xe6\\x98\\xaf\\xe8\\xb0\\x81\\xef\\xbc\\x9f";
         //
@@ -22,8 +26,35 @@ public class Test2 {
 //            System.out.println(s1.getBytes()[i]);
             bs[i] = (byte) 0xe6;
         }
-        t2(s);
+        writeHexByte("\"\"中\"".getBytes());
     }
+
+
+    private static void writeHexByte(byte[] b) throws IOException {
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < b.length; i++) {
+            int temp1=(b[i] & 0xf0) >> 4;//byte转16进制
+            char temp11=bin2char(temp1);
+            int temp111=(int)temp11;
+            ///////////////////////////////////
+            int temp2=b[i] & 0x0f;//byte转16进制
+            char temp22=bin2char(temp2);
+            int temp222=(int)temp22;
+            s.append(temp11);
+        }
+        System.out.println(s);
+    }
+
+    private static char bin2char(int bin) {
+        char c;
+        c=(char) (bin < 10 ? bin + '0' : bin - 10 + 'A');
+        return c;
+    }
+
+
+
+
+
 
 
     public static void t2(String str1) {//字符串转换为ASCII码
@@ -47,4 +78,89 @@ public class Test2 {
     }
 
 
+
+    public static String encode(String str){
+        List<String> list = new ArrayList<String>();
+        for (int i = 0; i < str.length(); i++) {
+            list.add(str.substring(i, i+1));
+        }
+        StringBuilder sb = new StringBuilder();
+        int len = list.size();
+        for (int i = 0; i < len; i++) {
+            sb.append("_");
+            if("_".equals(list.get(i))){
+                sb.append("\\UL");
+            }else if(list.get(i).matches("^[0-9]$")){
+                Integer v = Integer.valueOf(list.get(i));
+                if(i<len-1&&v>0){
+                    while(v>=0){
+                        sb.append(list.get(i+1));
+                        v--;
+                    }
+                }else{
+                    sb.append(list.get(i));
+                }
+            }else{
+                sb.append(list.get(i));
+            }
+        }
+        sb.delete(0, 1);
+        return sb.toString();
+    }
+    /**
+     * 1.如果有\UL 则转为_；
+     * 2.如果是重复的叠字，则转化为长度-1；
+     * @param str
+     * @return
+     */
+    public static String decode(String str){
+
+        String[] s = str.split("_");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length; i++) {
+            if(s[i].equals("\\UL")){
+                sb.append("_");
+            }else if(isSame(s[i])){
+                sb.append(changeFromSame(s[i]));
+            }else if("".equals(s[i])){
+                int j=-1;
+                while(i<(s.length-1)&&"".equals(s[i++])){
+                    j++;
+                }
+                i--;
+                sb.append(j-1).append("_");
+            }else{
+                sb.append(s[i]);
+            }
+        }
+
+        return sb.toString();
+    }
+    /**
+     * 如果是叠字，返回true，否则，返回false
+     * @param str
+     * @return
+     */
+    public static boolean isSame(String str){
+        if(str == null || str.length() < 2){
+            return false;
+        }
+        char[] c = str.toCharArray();
+        for (int i = 0; i < c.length-1; i++) {
+            if(c[i] != c[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 将叠字转化为指定类型的值
+     * @param str
+     * @return
+     */
+    public static String changeFromSame(String str){
+        Integer len = str.length()-1;
+        return len.toString();
+    }
 }
--- src/main/java/com/dfire/HexToStr.java
+++ src/main/java/com/dfire/HexToStr.java
@@ -1,12 +1,9 @@
 package com.dfire;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.hive.ql.exec.UDF;
 
 import java.io.UnsupportedEncodingException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.TimeZone;
+import java.net.URLDecoder;
 
 /**
  * @author <a href="mailto:huoguo@2dfire.com">火锅</a>
@@ -14,34 +11,31 @@ import java.util.TimeZone;
  */
 public class HexToStr extends UDF {
 
-    public static String changeStr(String str){
-        str = str.replaceAll("x","");
-        str = str.replaceAll("\\\\","");
+    public static String changeStr(String str) throws UnsupportedEncodingException {
+        //todo 需要优化
+        str = str.replaceAll("%", "%25");
+        str = str.replaceAll("\\\\x", "%");
+        String decode = URLDecoder.decode(str, "utf-8");
+        return decode;
 
-        byte[] b = new byte[str.length() / 2];
-        for (int i = 0; i < b.length; i++) {
-            // 将字符串每两个字符做为一个十六进制进行截取
-            String a = str.substring(i * 2, i * 2 + 2);
-            b[i] = (byte) Integer.parseInt(a, 16);
-        }
+    }
 
+
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        String s0 = "\\x22220\\xE7\\x89\\x9B\\xE6\\xB2\\xB9\\xE6\\x9E\\x9C\\x22";
+        s0 = "100.121.134.224 10.111.7.92 - [28/May/2019:07:18:04 +0800] app.dailuobo.com \"POST /getSearchContent.cgi HTTP/1.1\" 200 84 0.005 \"10.111.7.127:4567\" \"0.005\" \"-\" \"Dalvik/2.1.0 (Linux; U; Android 8.1.0; PAFM00 Build/OPM1.171019.026)\" \"183.160.100.110\" {\\x22IDFA\\x22:\\x22\\x22,\\x22IMEI\\x22:\\x22867278049761590\\x22,\\x22appVersion\\x22:\\x223.0.1\\x22,\\x22cityId\\x22:\\x2230\\x22,\\x22deveiceModel\\x22:\\x22PAFM00\\x22,\\x22deveiceToken\\x22:\\x2288KwF/NjxGBcz1kLIJSLg5aCbuaqCx/OKpyFnI1mcpY\\x5Cu003d\\x22,\\x22isDebug\\x22:\\x220\\x22,\\x22latitude\\x22:\\x2231.911211\\x22,\\x22longitude\\x22:\\x22117.327551\\x22,\\x22netType\\x22:\\x22wifi\\x22,\\x22shopId\\x22:\\x22522\\x22,\\x22system\\x22:\\x22android\\x22,\\x22version\\x22:\\x228.1.0\\x22,\\x22data\\x22:{\\x22productName\\x22:\\x22\\xE7\\x95\\x85\\xE6\\x84\\x8F100%\\x22}}";
+        System.out.println(s0);
+        System.out.println(changeStr(s0));
+
+    }
+
+    public String evaluate(String str) {
         try {
-            // 将字节数字以utf-8编码以字符串形式输出
-            return new String(b, "UTF-8");
+            return changeStr(str);
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
-        throw new IllegalArgumentException("error str :"+str);
-    }
-
-
-    public static void main(String[] args) {
-        String s = "\\x22\\xE7\\x89\\x9B\\xE6\\xB2\\xB9\\xE6\\x9E\\x9C\\x22";
-        System.out.println(changeStr(s));
-    }
-
-    public  String evaluate(String str){
-        return changeStr(str);
+        throw new IllegalArgumentException("error : " + str);
     }
 
 }
