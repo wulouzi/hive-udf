604695c41f12a460e235d482e2a1509a72fcc905
ranqiqiang <qiqiang@ran.gmail.com>
2019-05-31 16:28:11 +0800

    tocken 处理

--- src/main/java/com/dfire/ChangeNgLog.java
+++ src/main/java/com/dfire/ChangeNgLog.java
@@ -28,7 +28,6 @@ public class ChangeNgLog extends UDF {
         str = URLDecoder.decode(str, "utf-8");
         str = filterUnicode(str, emoJi, "");
         return str;
-
     }
 
 
@@ -36,7 +35,9 @@ public class ChangeNgLog extends UDF {
         String s0 = "\\x22\\xE7\\x89\\x9B\\xE6\\xB2\\xB9\\xE6\\x9E\\x9C\\x22";
         String s1 = "{100.121.134.234 10.111.0.251 - [28/May/2019:21:41:28 +0800] app.dailuobo.com \"POST /getAd.cgi HTTP/1.1\" 200 264 0.014 \"10.111.7.67:4567\" \"0.014\" \"-\" \"okhttp/3.8.1\" \"112.32.94.90\" {\\x22deveiceModel\\x22:\\x22vivo X9\\x22,\\x22longitude\\x22:\\x22117.383997\\x22,\\x22IDFA\\x22:\\x22\\x22,\\x22system\\x22:\\x22android\\x22,\\x22deveiceToken\\x22:\\x227+m8cxTPpBF9NzAW\\x5C/XeOeqQdJT2vKPxYxQbcjKVCj8U=\\x22,\\x22shopId\\x22:\\x22179\\x22,\\x22IMEI\\x22:\\x22866042030886514\\x22,\\x22appVersion\\x22:\\x223.0.1\\x22,\\x22isDebug\\x22:\\x220\\x22,\\x22version\\x22:\\x227.1.2\\x22,\\x22latitude\\x22:\\x2231.877184\\x22,\\x22netType\\x22:\\x22wifi\\x22,\\x22cityId\\x22:\\x2230\\x22,\\x22data\\x22:{}}\n";
         String s2 = "100.121.136.5 10.111.101.38 - [28/May/2019:17:55:01 +0800] _ \"HEAD / HTTP/1.0\" 200 0 0.000 \"-\" \"-\" \"-\" \"-\" \"-\"";
-        System.out.println(getNginxLog(s1));
+        String s3 = "61.219.11.153 10.111.143.208 - [29/May/2019:11:48:24 +0800] _ \"\\x01\\x02\\x03\\x04\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\" 400 591 0.000 \"-\" \"-\" \"-\" \"-\" \"-\"\n";
+
+        System.out.println(getNginxLog(s2));
 
     }
 
@@ -48,27 +49,31 @@ public class ChangeNgLog extends UDF {
         String[] f2 = org[2].split(" ");
         String[] f11 = org[11].split(",");
 
-        object.put("remote_addr", f0[0]);
-        object.put("server_addr", f0[1]);
-        object.put("remote_user", f0[2]);
-        object.put("time_local", f0[3] + " " + f0[4]);
-        object.put("domain", f0[5]);
-        object.put("method", f1[0]);
-        object.put("request", f1[1]);
-        object.put("http_version", f1[2]);
-        object.put("status", f2[1]);
-        object.put("body_byte_send", f2[2]);
-        object.put("request_time", f2[3]);
-        object.put("upstream_addr", org[3]);
-        object.put("upstream_response_time", org[5]);
-        object.put("referrer", org[7]);
-        object.put("agent", org[9]);
-        object.put("http_x_forwarded_for", f11[0]);
-        object.put("agent", org[9]);
-        if(org.length == 13){
-            object.put("post_message", changeStr(org[12]));
-        }
+        object.put("remote_addr", getMesage(f0,0));
+        object.put("server_addr", getMesage(f0,1));
+        object.put("remote_user", getMesage(f0,2));
+        object.put("time_local", getMesage(f0,3) + " " + getMesage(f0,4));
+        object.put("domain", getMesage(f0,5));
+        object.put("method", getMesage(f1,0));
+        object.put("request", getMesage(f1,1));
+        object.put("http_version", getMesage(f1,2));
+        object.put("status", getMesage(f2,1));
+        object.put("body_byte_send", getMesage(f2,2));
+        object.put("request_time", getMesage(f2,3));
+        object.put("upstream_addr", getMesage(org,3));
+        object.put("upstream_response_time", getMesage(org,5));
+        object.put("referrer", getMesage(org,7));
+        object.put("agent", getMesage(org,9));
+        object.put("http_x_forwarded_for", getMesage(f11,0));
+        object.put("post_message", changeStr(getMesage(org,12)));
         return object.toString();
+    }
+
+    private static String getMesage(String[] f,int index){
+        if(f.length - 1 >= index){
+            return f[index];
+        }
+        return "";
     }
 
 
---
+++ src/main/java/com/dlb/TokenChange.java
@@ -0,0 +1,108 @@
+package com.dlb;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import java.io.IOException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+/**
+ * dlb 专用
+ *
+ * @author <a href="mailto:huoguo@2dfire.com">火锅</a>
+ * @time 2019/5/29
+ */
+public class TokenChange extends UDF {
+
+
+    public static void main(String[] args) {
+        System.out.println(new TokenChange().parseToken("q7DJ23pztdomzr0/Huc gKXIhtjyPZ7fKpyFnI1mcpY="));
+    }
+
+
+    public String evaluate(String str) {
+        try {
+            return parseToken(str);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(str);
+        }
+        throw new IllegalArgumentException("error : " + str);
+    }
+
+    /**
+     * The Constant DES.
+     */
+    private final static String DES = "DES";
+
+    /**
+     * 分隔符
+     */
+    private final static String SEPARATOR = "|";
+
+    /**
+     * 加密key
+     */
+    private final static String encryptKey = "mallcaiMALLCAI";//ResourceBundle.getBundle("key").getString("encryptKey");
+
+
+    public String parseToken(String deveiceToken) {
+        deveiceToken = deveiceToken.replace(" ", "+");
+        return parse(deveiceToken);
+    }
+
+
+    static public String parse(String userDeveiceKey) {
+        try {
+            String tempKey = decrypt(userDeveiceKey, encryptKey);
+            String[] keys = tempKey.split("[" + SEPARATOR + "]");
+            return keys[0];
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static String decrypt(String data, String key) throws IOException,
+            Exception {
+        if (data == null)
+            return null;
+        byte[] buf = Base64.getDecoder().decode(data);
+        byte[] bt = decrypt(buf, key.getBytes());
+        return new String(bt);
+    }
+
+    /**
+     * 根据键值进行解密
+     */
+    private static byte[] decrypt(byte[] data, byte[] key) throws Exception {
+        Cipher cipher = cipherInit(data, key, Cipher.DECRYPT_MODE);
+        return cipher.doFinal(data);
+    }
+
+    private static Cipher cipherInit(byte[] data, byte[] key, int cipherValue)
+            throws Exception {
+        /** 生成一个可信任的随机数源 **/
+        SecureRandom sr = new SecureRandom();
+        /** 从原始密钥数据创建DESKeySpec对象 **/
+        DESKeySpec dks = new DESKeySpec(key);
+        /** 创建一个密钥工厂，然后用它把DESKeySpec转换成SecretKey对象 **/
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DES);
+        SecretKey securekey = keyFactory.generateSecret(dks);
+        /** Cipher对象实际完成加密或解密操作 **/
+        Cipher cipher = Cipher.getInstance(DES);
+        /** 用密钥初始化Cipher对象 **/
+        cipher.init(cipherValue, securekey, sr);
+        return cipher;
+    }
+
+
+}
+
